b816ad6d6b71e484e15eb6472144a703099b8a5e
Ängla Juliusson <angla02@icloud.com>
2026-03-12 12:19:59 +0100

    Registrera reviews-routes i server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors' // Importera cors
 import './config/db.js'
+import { routes as reviews } from './routes/reviews.js'; // Importera route-funktionerna från reviews.js
 
 const fastify = Fastify({ logger: true }); 
 
@@ -9,6 +10,9 @@ const fastify = Fastify({ logger: true });
 await fastify.register(cors, {
   origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] // Tillåt alla origins (utveckling) och alla metoder
 })
+
+// Registrera routes så att alla definierade endpoints blir tillgängliga
+fastify.register(reviews);
 
 // Run the server!
 try {
